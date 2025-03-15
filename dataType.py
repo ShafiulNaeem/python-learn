8012d13abe80a4fbcde8d4b6f44706990f43a363
ShafiulNaeem <shafiulnaeemrifat@gmail.com>
2025-03-16 00:11:08 +0600

    string learn

--- dataType.py
+++ dataType.py
@@ -11,3 +11,39 @@ print(complex(x))
 # random number
 import random
 print(random.randrange(100, 1000))
+
+# string type
+# single line string
+a = "single line string"
+# ADD NEW LINE before print
+print("\n"+ a)
+
+# multi line string
+b = """APPLE,
+BANANA,
+CHERRY
+"""
+print(b)
+
+# string array
+print(a[1])
+
+# loop through string
+for x in "banana":
+    print(x)
+
+# string length
+print(len(a))
+# check string
+
+if "APPLE" in b:
+    print("Yes, 'apple' is in the fruits string")
+
+# format string
+age = 27
+ageMonth = age * 12
+txt = f"My name is \"Rifat\", and I am {age:.2f}"
+text = "My name is Rifat, and I am {} years old. I am {} months old".format( 20,30)
+print(text)
+print(txt)
+
