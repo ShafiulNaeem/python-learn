d545ba5427abfe1b97311b002718a91ed0f39e80
ShafiulNaeem <shafiulnaeemrifat@gmail.com>
2025-03-15 23:26:30 +0600

    numeric data type

---
+++ dataType.py
@@ -0,0 +1,13 @@
+# number type
+# int, float, complex
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(float(x))
+print(int(y))
+print(complex(x))
+
+# random number
+import random
+print(random.randrange(100, 1000))
