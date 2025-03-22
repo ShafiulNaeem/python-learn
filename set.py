14d501c26c6586c53059f72c1bd4879ae83c8d43
ShafiulNaeem <shafiulnaeemrifat@gmail.com>
2025-03-22 13:35:57 +0600

    set: join, methods

--- set.py
+++ set.py
@@ -24,3 +24,48 @@ deldata.discard("JavaScript")
 # deldata.clear()
 print(deldata)
 # del deldata
+
+# set loop and comprehension
+print("=== set loop and comprehension ===")
+phpStack = set()
+for i in data:
+    print('loop:',i)
+    if i == "PHP" or i == "Laravel":
+        phpStack.add(i)
+
+
+otherData = {i for i in data if i not in phpStack}
+print("other data:", otherData)
+print("php stack:", phpStack)
+
+# set join
+print("=== set join ===")
+joinData = otherData.union(phpStack)
+
+# update
+a = {1,2,3}
+b = {1,2,4}
+a.update(b)
+print('update:',a)
+
+# intersection
+c  = a & b
+print('intersection:',c)
+
+# a.intersection_update(b)
+print('intersection update:',a)
+
+# difference
+d = a - b
+print('difference:',d)
+
+# a.difference_update(b)
+print('difference update:',a)
+
+# symmetric difference
+a.add(5)
+b.add(6)
+e = a ^ b
+print('symmetric difference:',e)
+
+print(joinData)
