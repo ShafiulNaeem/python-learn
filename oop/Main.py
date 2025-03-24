23441964ef64299af9162fa268099bcb06c98e01
ShafiulNaeem <shafiulnaeemrifat@gmail.com>
2025-03-24 17:19:47 +0600

    log inherit

--- oop/Main.py
+++ oop/Main.py
@@ -1,18 +1,18 @@
 from Log import Log
-class Main:
+class Main(Log):
     def __init__(self, filename):
-        self.logDta = Log(filename)
+        super().__init__(filename)
     
     @Log.logger
-    def test(self,oid):
-        self.logDta.log("Test")
+    def test(self,oid,op):
+        super().log("This is a log message")
         return "Man is a social animal"
 
 # Create an object of Log class
 
 data = Main("oop/main.log")
-print(data.test(4))
-print(data.logDta.read())
+print(data.test(4,9))
+print(data.read())
 # print(data.read())
 # print(data.log("This is a log message"))
     
